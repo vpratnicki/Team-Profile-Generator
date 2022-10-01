4e4151082a3829704ec70962cd130d91c21c83d4
vpratnicki <vanessa@pratnicki.net>
2022-09-30 20:51:59 -0400

    Chore: completed employee tests

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -10,12 +10,18 @@ const Employee = require('../lib/Employee.js');
 
 test('create employee name object', () => {
     const name = 'Charles';
-    const e = new Employee(name);
-    expect(e.name).toBe(name);
+    const employee = new Employee(name);
+    expect(employee.name).toBe(name);
 });
 
 test('create employee id object', () => {
     const testId = 350;
-    const e = new Employee('Charles', testId);
-    expect(e.id).toBe(testId);
+    const employee = new Employee('Charles', testId);
+    expect(employee.id).toBe(testId);
+});
+
+test('create employee email', () => {
+    const testEmail = 'meow@cat.net';
+    const employee = new Employee('Charles', 350, testEmail);
+    expect(employee.email).toBe(testEmail);
 });
