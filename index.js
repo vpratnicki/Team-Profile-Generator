ac0655a9c4f72e4929c9aa752b8f2613f5d633b6
vpratnicki <vanessa@pratnicki.net>
2022-10-01 17:29:56 -0400

    Chore: set up Inital user prompts

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
-const inquirer = require('inquire');
-const generatePage = require('./src/page-template');
-const { writeFile, copyFile } = require('./src/generate-site');
-const { Manager } = require('./lib/Manager');
+const inquirer = require('inquirer');
+// const generatePage = require('./src/page-template');
+// const { writeFile, copyFile } = require('./src/generate-site');
+// const Manager = require('./lib/Manager');
+
+const promptManagerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'manager.name',
+            message: "Enter the team manager's name"
+        },
+        {
+            type: 'input',
+            name: 'manager.id',
+            message: "enter the team manager's ID number"
+        },
+        {
+            type: 'input',
+            name: 'manager.email',
+            message: "enter the team manager's email address"
+        },
+        {
+            type: 'input',
+            name: 'mananager.officeNumber',
+            message: "enter the team manager's office number"
+        }
+    ])
+};
+
+const promptAdditionalEmployee = employeeData => {
+    console.log(`
+  =================
+  Add a Employee
+  =================
+  `);
 
 
 
+  promptManagerQuestions()
+  .then(promptAdditionalEmployee)
+  .then(employeeData => {
+    console.log(employeeData);
 
-
+    // const pageHTML = generatePage(portfolioData);
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+ });
+};
 
--- src/page-template.js
+++ src/page-template.js
@@ -1,10 +1,8 @@
 
-const generateManager = () => {
+const generateManager = (manager) => {
     return `
     <section class="my-3" id="about">
-      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-      <p>${manager.name}</p>
-      <p>${manager.role}</p>
+      <h2 class="text-dark bg-primary p-2 display-inline-block">${manager.name}<br/>${manager.role}</h2>
       <p>${manager.id}</p>
       <p>${manger.email}</p>
       <p>${manager.officeNumber}</p>
@@ -12,3 +10,4 @@ const generateManager = () => {
     </section>
   `;
 };
+
